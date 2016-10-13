0551ee0ae2e18c465e0aa41dfe7242ccea05a433
jinkia <379346631@qq.com>
2016-10-13 16:52:47 +0800

    权限管理

---
+++ app/src/main/java/com/example/jinkai/jkpermissonutil/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.jinkai.jkpermissonutil;
+
+import android.Manifest;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends PermissionActicity implements View.OnClickListener {
+    private Button btnPreSingle, btnPreMulti, btnFrag;
+    private FrameLayout fl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPreSingle = (Button) findViewById(R.id.btn_per_single);
+        btnPreMulti = (Button) findViewById(R.id.btn_pre_multi);
+        btnFrag = (Button) findViewById(R.id.btn_fragment);
+        fl = (FrameLayout) findViewById(R.id.fragment);
+
+        btnPreSingle.setOnClickListener(this);
+        btnPreMulti.setOnClickListener(this);
+        btnFrag.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_per_single:
+                checkPermission(new CheckPermListener() {
+                    @Override
+                    public void Granted() {
+                        Toast.makeText(MainActivity.this, "通过", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void Denied() {
+                        Toast.makeText(MainActivity.this, "失败", Toast.LENGTH_SHORT).show();
+                        //检查是否选择了不再提醒
+                    }
+                }, "请求获取照相机权限", Manifest.permission.CAMERA);
+                break;
+            case R.id.btn_pre_multi:
+                checkPermission(new CheckPermListener() {
+                    @Override
+                    public void Granted() {
+                        Toast.makeText(MainActivity.this, "通过", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void Denied() {
+                        Toast.makeText(MainActivity.this, "失败", Toast.LENGTH_SHORT).show();
+                        //如果选择不在提示
+                    }
+                }, "请求获取照相机和读取文件权限", Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                break;
+            case R.id.btn_fragment:
+                fl.removeAllViews();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new MainActivityFragment()).commit();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jinkai/jkpermissonutil/MainActivityFragment.java
@@ -0,0 +1,71 @@
+package com.example.jinkai.jkpermissonutil;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    private Button btnPreSingle, btnPreMulti;
+
+    public MainActivityFragment() {
+    }
+
+    View mRootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.fragment_main, container, false);
+        btnPreSingle = (Button) mRootView.findViewById(R.id.btn_per_single);
+        btnPreMulti = (Button) mRootView.findViewById(R.id.btn_pre_multi);
+        btnPreSingle.setOnClickListener(this);
+        btnPreMulti.setOnClickListener(this);
+        return mRootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_per_single:
+                ((PermissionActicity) getActivity()).checkPermission(new PermissionActicity.CheckPermListener() {
+                    @Override
+                    public void Granted() {
+                        Toast.makeText(getContext(), "通过", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void Denied() {
+                        Toast.makeText(getContext(), "失败", Toast.LENGTH_SHORT).show();
+                        //检查是否选择了不再提醒
+                    }
+                }, "请求获取照相机权限", Manifest.permission.CAMERA);
+                break;
+            case R.id.btn_pre_multi:
+                ((PermissionActicity) getActivity()).checkPermission(new PermissionActicity.CheckPermListener() {
+                    @Override
+                    public void Granted() {
+                        Toast.makeText(getContext(), "通过", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void Denied() {
+                        Toast.makeText(getContext(), "失败", Toast.LENGTH_SHORT).show();
+                        //如果选择不在提示
+                    }
+                }, "请求获取照相机和读取文件权限", Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                break;
+        }
+    }
+}
